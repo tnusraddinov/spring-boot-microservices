882ae8260fdbf18e8e6a0d07f20f6afb8a10fc4b
Tamerlan N <tamerlan5@hotmail.com>
2022-05-30 20:22:22 +0300

    resilience4j retry property added

--- order-service/src/main/java/com/tna/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/tna/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.tna.orderservice.controller;
 import com.tna.orderservice.dto.CreateOrderRequest;
 import com.tna.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import java.util.concurrent.CompletableFuture;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     @TimeLimiter(name = "inventory")
+    @Retry(name="inventory")
     public CompletableFuture<String> placeOrder(@RequestBody CreateOrderRequest request) {
         return CompletableFuture.supplyAsync(() -> orderService.placeOrder(request));
     }
